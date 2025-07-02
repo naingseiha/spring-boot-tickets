c783688531793ed5d5afd68fbc83f16b8639142d
Seiha <naingseiha@gmail.com>
2025-07-03 00:37:29 +0200

    CreateEventRequestDto, CreateEventResponseDto, CreateTicketTypeRequestDto, CreateTicketTypeResponseDto

---
+++ src/main/java/com/borntocode/dev/tickets/domain/dtos/CreateEventRequestDto.java
@@ -0,0 +1,40 @@
+package com.borntocode.dev.tickets.domain.dtos;
+
+import com.borntocode.dev.tickets.domain.entities.EventStatusEnum;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateEventRequestDto {
+
+    @NotBlank(message = "Event name is required")
+    private String name;
+
+    private LocalDateTime start;
+
+    private LocalDateTime end;
+
+    @NotBlank(message = "Venue is required")
+    private String venue;
+
+    private LocalDateTime saleStart;
+
+    private LocalDateTime saleEnd;
+
+    @NotNull(message = "Event status is required")
+    private EventStatusEnum status;
+
+    @NotEmpty(message = "At least one ticket type is required")
+    @Valid
+    private List<CreateTicketTypeRequestDto> ticketTypes;
+}
